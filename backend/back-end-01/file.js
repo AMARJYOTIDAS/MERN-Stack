860c110c89a329f67e0f04ea2a439f32a0741204
AMARJYOTIDAS <hd665869@gmail.com>
2026-09-20 23:43:14 +0530

    new changes

--- backend/back-end-01/file.js
+++ backend/back-end-01/file.js
@@ -1,15 +1,52 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("home page");
-  } else if (req.url === "/about") {
-    res.write("about page");
-  } else if (req.url === "/contact") {
-    res.write("contact page");
-  }
-  res.end();
-});
+const fs = require("fs");
 
-server.listen(8000, () => {
-  console.log("server running on localhost:8000");
-});
+// sync...
+// fs.writeFileSync("./test.txt", "hello node js");
+
+//asyncronous file
+// fs.writeFile("./test.txt", "hello node js this is async", (err) => {});
+
+//sunc..
+// const result = fs.readFileSync("./contact.txt", "utf-8");
+// console.log(result);
+
+//Asnc ...
+// fs.readFile("./contact.txt", "utf-8", (err, result) => {
+//   if (err) {
+//     console.log("error", err);
+//   } else {
+//     console.log(result);
+//   }
+// });
+
+// fs.appendFileSync("./test.txt", new Date().getDate().toLocaleString());
+// fs.appendFileSync("./test.txt", `\n${Date.now()} append this`);
+
+// fs.unlink("./contact.txt", (err) => {
+//   if (err) {
+//     console.log("err", err);
+//   } else {
+//     console.log("delete succesfuly");
+//   }
+// });
+
+// fs.mkdirSync("FolderBy_fs");
+// fs.rmdirSync("./FolderBy_fs");
+
+console.log(fs.statSync("./test.txt"));
+
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.write("home page");
+//   } else if (req.url === "/about") {
+//     res.write("about page");
+//   } else if (req.url === "/contact") {
+//     res.write("contact page");
+//   }
+//   res.end();
+// });
+
+// server.listen(8000, () => {
+//   console.log("server running on localhost:8000");
+// });
--- backend/back-end-01/index.js
+++ backend/back-end-01/index.js
@@ -1,4 +1,8 @@
 // require("./file");
 // require("./query");
 // require("./total_price");
-require("./student_failpass");
+// require("./student_failpass");
+const { add, sub } = require("./Math");
+console.log(
+  `the addition of two no is  ${add(1, 2)}, substarction of two no is ${sub(3, 2)}`,
+);
