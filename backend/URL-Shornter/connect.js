f1115987e89b19cc4c1235afcb70b0c00c21c4f1
AMARJYOTIDAS <hd665869@gmail.com>
2026-09-26 23:57:58 +0530

    new changes

--- backend/URL-Shornter/connect.js
+++ backend/URL-Shornter/connect.js
@@ -1,3 +1,10 @@
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 async function connectToMongoDB(url) {
